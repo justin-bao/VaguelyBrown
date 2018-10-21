48f4c1d1246cb2b1dc8b33cc2820d7fd7eb4f037
Marcus Amalachandran <markamal@uw.edu>
2018-10-20 23:18:41 -0700

    more stuff

---
+++ myscript.js
@@ -0,0 +1,9 @@
+// Checking page title
+if (true) {
+    //Creating Elements
+    var btn = document.createElement("BUTTON")
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+    //Appending to DOM
+    document.head.appendChild(btn);
+}

9ff7989cfd4d9e47308e78c7553182c3e9046a48
Saurav Pahadia <sauravpahadia@gmail.com>
2018-10-21 06:27:18 -0700

    added oauth user verification

--- background.js
+++ background.js
@@ -1,13 +1,20 @@
 
 /* A function creator for callbacks */
 function updateDatabase(imgURL) {
+alert("Token: " + authToken + " ImgURL: " + imgURL + " Timestamp: " + Date.now());
+
   //$.post('localhost:3000/postLikedImage', {
-  //  userID: 'ID HERE',
+  //  userID: 'authToken',
   //  imageURL: imgURL,
   //  timestamp: Date.now()
   //})
 }
 
+var authToken;
+chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+        // Use the token.
+        authToken = token;
+            });
 /* When the browser-action button is clicked... */
 chrome.browserAction.onClicked.addListener(function(tab) {
     /*...check the URL of the active tab against our pattern and... */
